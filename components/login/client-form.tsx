4159f3a9425a042ba5cbf39f1cf7b67de5692fcc
ZineddineBk09 <eino1209@gmail.com>
2024-03-24 12:33:35 +0100

    client form submit logic

--- components/login/client-form.tsx
+++ components/login/client-form.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -15,27 +14,35 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { useRouter } from 'next/navigation'
+import { signIn } from '@/lib/auth'
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
+  clientId: z.string().min(1),
+  clientSecret: z.string().min(1),
 })
 
 export function ClientForm() {
-  // 1. Define your form.
+  const router = useRouter()
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: '',
+      clientId: '',
+      clientSecret: '',
     },
   })
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    await signIn({
+      clientId: values.clientId,
+      clientSecret: values.clientSecret,
+    }).then((res: { ok: boolean; status: number } | any) => {
+      console.log('res:', res)
+      if (res.ok && res.status == 200) {
+        router.push('/dashboard')
+      }
+    })
   }
 
   return (
@@ -43,15 +50,31 @@ export function ClientForm() {
       <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
         <FormField
           control={form.control}
-          name='username'
+          name='clientId'
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Client ID</FormLabel>
               <FormControl>
-                <Input placeholder='shadcn' {...field} />
+                <Input placeholder='client identifier' {...field} />
               </FormControl>
               <FormDescription>
-                This is your public display name.
+                This is the public identifier for your client.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='clientSecret'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Client Secret</FormLabel>
+              <FormControl>
+                <Input placeholder='********' {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the secret key for your client.
               </FormDescription>
               <FormMessage />
             </FormItem>
