96caadbeb122a6c10b2ee8cd29107435bd713d11
ZineddineBk09 <eino1209@gmail.com>
2024-03-24 12:32:05 +0100

    client login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { ClientForm } from "@/components/login/client-form";
+import { ClientForm } from '@/components/login/client-form'
 
 export default async function LoginPage() {
   return (
     <div className='container mx-auto py-10'>
-      <h1 className='text-3xl font-bold'>Users</h1>
+      <h1 className='text-3xl font-bold mb-10'>Client Login</h1>
       <ClientForm />
     </div>
   )
