2118e1576e407135e98d653935b38d7af9d70c47
ZineddineBk09 <eino1209@gmail.com>
2024-03-28 16:38:02 +0100

    fix

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -6,7 +6,7 @@ export const getAccessToken = async () => {
     const url = `${process.env.NEXT_PUBLIC_KEYCLOAK_URL}/protocol/openid-connect/token`
 
     const response = await fetch(
-      'http://192.168.1.37:8080/realms/master/protocol/openid-connect/token',
+      'http://localhost:8080/realms/master/protocol/openid-connect/token',
       {
         method: 'POST',
         headers: {
@@ -51,76 +51,41 @@ export const createUser = async (user: any, endpoint: string) => {
   }
 }
 
-export const updateRecord = async (record: any, endpoint: string) => {
+export const deleteUser = async (userId: string, accessToken: string) => {
   try {
-    const response = await axios.put(endpoint + '/' + record.id + '/', record)
+    const response = await axios.delete(
+      `/admin/realms/master/users/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
     const data = await response.data
     return data
-  } catch (error) {
+  } catch (error: any) {
     throw error
   }
 }
 
-export const partialUpdateRecord = async (record: any, endpoint: string) => {
+export const updateUser = async (
+  user: any,
+  userId: string,
+  accessToken: string
+) => {
   try {
-    const response = await axios.patch(endpoint + '/' + record.id + '/', record)
+    const response = await axios.put(
+      `/admin/realms/master/users/${userId}`,
+      user,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
     const data = await response.data
     return data
-  } catch (error) {
+  } catch (error: any) {
     throw error
-  }
-}
-
-export const deleteRecord = async (id: string | number, endpoint: string) => {
-  try {
-    const response = await axios.delete(endpoint + '/' + id + '/')
-    const data = await response.data
-    return data
-  } catch (error) {
-    throw error
-  }
-}
-
-export const getRecords = async (endpoint: string) => {
-  try {
-    const response = await axios.get(endpoint + '/')
-    const data = await response.data
-    return data
-  } catch (error) {
-    return []
-  }
-}
-
-export const getRecord = async (id: string, endpoint: string) => {
-  try {
-    const response = await axios.get(endpoint + '/' + id + '/')
-    const data = await response.data
-    return data
-  } catch (error) {
-    return {}
-  }
-}
-
-export const filterRecords = async (params: any, endpoint: string) => {
-  try {
-    const response = await axios.get(endpoint + '/', {
-      params,
-    })
-    const data = await response.data
-    return data
-  } catch (error) {
-    return []
-  }
-}
-
-export const searchRecords = async (search: string, endpoint: string) => {
-  try {
-    const response = await axios.get(endpoint + '/', {
-      params: { search },
-    })
-    const data = await response.data
-    return data
-  } catch (error) {
-    return []
   }
 }
