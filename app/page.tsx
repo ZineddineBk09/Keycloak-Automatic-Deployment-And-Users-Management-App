0296909e480f592b2ad0d2998e84fd79cb7ebe18
ZineddineBk09 <eino1209@gmail.com>
2024-04-06 16:13:42 +0100

    remove auth contxt

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ import { UsersContextProvider } from '../context/csv'
 
 export default function MainPage() {
   return (
-    <AuthProvider>
-      <UsersContextProvider>
-        <div className='container mx-auto py-10'>
-          <h1 className='text-3xl font-bold mb-10'>Upload CSV File</h1>
-          <CsvReader />
-        </div>
-      </UsersContextProvider>
-    </AuthProvider>
+    <UsersContextProvider>
+      <div className='container mx-auto py-10'>
+        <h1 className='text-3xl font-bold mb-10'>Upload CSV File</h1>
+        <CsvReader />
+      </div>
+    </UsersContextProvider>
   )
 }
