ee429c525a82736118f48b78606824adcc85f8f0
ZineddineBk09 <eino1209@gmail.com>
2024-04-06 16:13:23 +0100

    fix

--- app/api/login/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-import { prisma } from '../../../db'
-
-// handle the logic for the POST request where the client send hist clientId/clientSecret to get an access token form the keycloak server
-// http://localhost:8080/realms/master/protocol/openid-connect/token
-
-export async function POST(request: NextRequest) {}
