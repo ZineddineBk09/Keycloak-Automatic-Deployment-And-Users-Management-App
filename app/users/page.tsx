20979ace528cbcfeb9d773ce50aa3784a7ff18c5
ZineddineBk09 <eino1209@gmail.com>
2024-04-07 14:29:01 +0100

    users page

---
+++ app/users/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { columns } from '../../components/users/columns'
+import { DataTable } from '../../components/users/data-table'
+import { UsersContextProvider } from '../../context/users'
+
+export default function MainPage() {
+  return (
+    <>
+      <UsersContextProvider>
+        <div className='container mx-auto py-10'>
+          <h1 className='text-3xl font-bold mb-10'>Keycloak Users</h1>
+          <DataTable columns={columns} />
+        </div>
+      </UsersContextProvider>
+    </>
+  )
+}
