f38ef9a07140cebe5691a72af18ecc2d66f6bad9
ZineddineBk09 <eino1209@gmail.com>
2024-04-07 14:25:27 +0100

    add clientId and clientSecret fields for client login & add responses

--- app/api/client/login/route.ts
+++ app/api/client/login/route.ts
@@ -1,21 +1,13 @@
 import { NextResponse, NextRequest } from 'next/server'
 import { prisma } from '../../../../db'
 import bcrypt from 'bcrypt'
-// create a validator for the request body with zod
 import { z } from 'zod'
 
 // define the schema for the request body
 const schema = z.object({
   clientId: z.string().min(1),
   clientSecret: z.string().min(1),
-  realmId: z.string().min(1),
-  authProtocol: z.string().min(1),
-  adminUser: z.string().min(1),
-  serverUrl: z.string().min(1),
 })
-
-// handle the logic for the POST request where the client send hist clientId/clientSecret to get an access token form the keycloak server
-// http://localhost:8080/realms/master/protocol/openid-connect/token
 
 export async function POST(request: NextRequest) {
   const req = await request.json()
@@ -23,13 +15,21 @@ export async function POST(request: NextRequest) {
   // check if the request body is valid
   const result = schema.safeParse(req)
 
-  // if the request body is not valid
-  if (!result.success) {
+  // if the request body is not valid or contains more than 2 keys
+  if (Object.keys(req).length !== 2 || !result.success) {
     console.log('invalid request body')
-    return NextResponse.error()
+    return NextResponse.json(
+      {
+        status: 400,
+        data: {
+          message: 'invalid request body',
+        },
+      },
+      { status: 400 }
+    )
   }
 
-  const { clientId, clientSecret, realmId } = req
+  const { clientId, clientSecret } = req
 
   // check if the client exist in the database
   let client = await prisma.client.findUnique({
@@ -41,18 +41,16 @@ export async function POST(request: NextRequest) {
   // if the client does not exist
   if (!client) {
     console.log('client does not exist')
-    try {
-      client = await prisma.client.create({
+
+    return NextResponse.json(
+      {
+        status: 404,
         data: {
-          clientId,
-          clientSecret: await bcrypt.hash(clientSecret, 10),
-          realmId,
+          message: 'client does not exist',
         },
-      })
-    } catch (err) {
-      console.log('error creating client', err)
-      return NextResponse.error()
-    }
+      },
+      { status: 404 }
+    )
   }
 
   // if the client exist and the clientSecret is correct
@@ -71,10 +69,26 @@ export async function POST(request: NextRequest) {
     })
 
     // return the response from the keycloak server
-    return NextResponse.json(await response.json())
+    return NextResponse.json(
+      {
+        status: response.status,
+        data: await response.json(),
+      },
+      {
+        status: response.status,
+      }
+    )
   } else {
     // return an error
     console.log('clientSecret is incorrect')
-    return NextResponse.error()
+    return NextResponse.json(
+      {
+        status: 401,
+        data: {
+          message: 'client secret is incorrect',
+        },
+      },
+      { status: 401 }
+    )
   }
 }
