d5684ea105752440495d7b9e07040be1d18b2d87
ZineddineBk09 <eino1209@gmail.com>
2024-04-08 11:36:24 +0100

    refactor

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,7 +3,7 @@ import { columns } from '../../components/users/columns'
 import { DataTable } from '../../components/users/data-table'
 import { UsersContextProvider } from '../../context/users'
 
-export default function MainPage() {
+export default function UsersPage() {
   return (
     <>
       <UsersContextProvider>
