a53c35ca893c6c151c7aaf97540eeb55eefb1144
ZineddineBk09 <eino1209@gmail.com>
2024-04-11 11:10:15 +0100

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { ClientLoginForm } from '../../components/client/login-form'
+import { ClientLoginForm } from '../../components/forms/login-form'
 
 export default function LoginPage() {
   return (
