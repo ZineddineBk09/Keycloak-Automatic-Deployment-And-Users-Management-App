098bdcd7391bd9ffec1ac26717935a6d307ae6db
ZineddineBk09 <eino1209@gmail.com>
2024-04-14 11:08:47 +0100

    custom edit dialog

---
+++ components/clients/dialogs/edit.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import { Button } from '../../ui/button'
+import { KeycloakClient, KeycloakUser } from '../../../interfaces'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '../../ui/dialog'
+import { Input } from '../../ui/input'
+import { Label } from '../../ui/label'
+import { useState } from 'react'
+import { updateRecord } from '../../../lib/api'
+import { toast } from 'sonner'
+import { useUsersContext } from '../../../context/users'
+
+function EditDialog({ client }: { client: KeycloakClient }) {
+  const { fetchUsers } = useUsersContext()
+  const [fields, setFields] = useState({
+    name: client.name,
+  })
+
+  const handleUpdate = async () => {
+    await updateRecord('clients', fields, client.id)
+      .then(() => {
+        toast.success('User updated successfully')
+        fetchUsers()
+      })
+      .catch((error) => {
+        toast.error('Error updating client')
+        console.error(error)
+      })
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <span className='relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-slate-100 hover:bg-slate-100 focus:text-slate-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-800 dark:focus:text-slate-50'>
+          Edit
+        </span>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[550px]'>
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to client here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='grid gap-4 py-4'>
+          {['username', 'firstName', 'lastName', 'email'].map((field) => {
+            return (
+              <div className='grid grid-cols-4 items-center gap-4' key={field}>
+                <Label htmlFor={field} className='text-right'>
+                  {field}
+                </Label>
+                <Input
+                  id={field}
+                  // @ts-ignore
+                  defaultValue={fields[field]}
+                  onChange={
+                    // disable onchange for username
+                    field === 'username'
+                      ? () => {}
+                      : (event) => {
+                          setFields((prev: any) => ({
+                            ...prev,
+                            [field]: event.target.value,
+                          }))
+                        }
+                  }
+                  disabled={field === 'username'}
+                  className='col-span-3'
+                />
+              </div>
+            )
+          })}
+        </div>
+        <DialogFooter className=''>
+          <DialogClose>
+            <Button variant='outline'>Close</Button>
+          </DialogClose>
+          <Button type='submit' onClick={handleUpdate}>
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default EditDialog
