a45c9d5e21e4abbde9fc4ec5554c65078af2731e
ZineddineBk09 <eino1209@gmail.com>
2024-04-17 13:19:21 +0100

    upload csv file page context

--- context/csv.tsx
+++ context/csv.tsx
@@ -1,7 +1,5 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import { User } from '../interfaces'
-import { useAuth } from './auth-context'
-import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
 import { createUser } from '../lib/api'
 
@@ -22,48 +20,17 @@ export const UsersContextProvider = ({
   children: React.ReactNode
 }) => {
   const [users, setUsers] = useState<User[]>([] as User[])
-  const { session } = useAuth()
   const router = useRouter()
-
-  // const createUser = async (user: User) => {
-  //   // call the create user API
-  //   try {
-  //     const response = await fetch(
-  //       process.env.NEXT_PUBLIC_KEYCLOAK_USERS_URL || '',
-  //       {
-  //         method: 'POST',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //           Authorization: `Bearer ${session.access_token}`,
-  //         },
-  //         body: JSON.stringify(user),
-  //       }
-  //     )
-  //     if (!response.ok) {
-  //       toast.error(`Failed to create user ${user.username}`)
-  //       throw new Error('Failed to create user')
-  //     }
-
-  //     toast.success(`User ${user.username} created`)
-  //     return {
-  //       status: response.status,
-  //       message: 'User created',
-  //     }
-  //   } catch (error: any) {
-  //     console.error('Error creating user:', error)
-  //     throw error
-  //   }
-  // }
 
   const uploadToKeycloak = async () => {
     try {
       const createdUsers = await Promise.all(
         users.map(async (user) => {
-          return await createUser(user, session.access_token)
+          return await createUser(user)
         })
       )
       setUsers([])
-      router.push('/dashboard')
+      router.push('/users')
     } catch (error: any) {
       console.error('Error uploading users:', error)
     }
