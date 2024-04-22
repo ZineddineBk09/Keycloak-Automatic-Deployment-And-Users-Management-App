6a0f1b7c70464ebb979f7f344881f21c79caf629
ZineddineBk09 <eino1209@gmail.com>
2024-04-22 09:06:11 +0100

    record details dialog

---
+++ components/shared/dialogs/details.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { Button } from '../../ui/button'
+import { KeycloakClient, KeycloakUser } from '../../../interfaces'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '../../ui/dialog'
+import JSONPretty from 'react-json-pretty'
+
+function DetailsDialog({ data }: { data: KeycloakUser | KeycloakClient }) {
+  
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <span className='relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-slate-100 hover:bg-slate-100 focus:text-slate-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-800 dark:focus:text-slate-50'>
+          View
+        </span>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Details</DialogTitle>
+          <DialogDescription>
+            View details here. Click close when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='overflow-y-auto max-h-[600px]'>
+          <JSONPretty data={data} />
+        </div>
+        <DialogFooter>
+          <DialogClose>
+            <Button>Close</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default DetailsDialog
