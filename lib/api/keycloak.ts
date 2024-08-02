6755e5a76d4c058f314d2652d9aa420872a80c19
ZineddineBk09 <eino1209@gmail.com>
2024-08-02 23:07:11 +0100

    updates

--- lib/api/keycloak.ts
+++ lib/api/keycloak.ts
@@ -54,9 +54,9 @@ export const createUser = async (user: User) => {
     });
     const data = await response.data;
 
-    if (response.status === 201) {
-      toast.success(`User ${user.username} created`);
-    }
+    // if (response.status === 201) {
+      // toast.success(`User ${user.username} created`);
+    // }
     return data;
   } catch (error: any) {
     toast.error(`Failed to create user ${user.username}`);
@@ -162,3 +162,87 @@ const getKcSession = () => {
     .find((cookie) => cookie.includes("kc_session"))
     ?.split("=")[1] as string;
 };
+
+// return users count : http://127.0.0.1:8080/admin/realms/master/users/count
+// response will be a number: 100
+export const getUsersCount = async () => {
+  const kcSession = getKcSession();
+  const { domain, realm, admin } = await getClientDomainRealmAdminAndProtocol();
+
+  if (!kcSession || !realm || !admin) {
+    throw new Error(
+      "Error fetching users count. Please check if the server is running. and try again."
+    );
+  }
+
+  try {
+    const response = await axios.get(`/${admin}/realms/${realm}/users/count`, {
+      baseURL: domain,
+      headers: {
+        Authorization: `Bearer ${kcSession}`,
+      },
+    });
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Reset password a user password
+// first the admin needs to have role "admin" to be able to reset password
+// ex: decoded token
+/**
+ * .....
+ * "realm_access": {
+    "roles": [
+      "create-realm",
+      "default-roles-master",
+      "offline_access",
+      "admin", // this is the role we need to check for
+      "uma_authorization"
+    ]
+  },
+  .....
+ */
+// we need to check for "admin" role first (decoded the access token) before we can reset password 
+// if role present, we can reset password with a put request to the user endpoint
+// ex endpoint: http://10.0.0.95/admin/realms/master/users/7d8e81a7-551a-4085-b101-21ce4c8dbb9e/reset-password
+// ex body: { "type": "password", "temporary": false, "value": "password" }
+export const resetUserPassword = async (userId: string, password: string) => {
+  const kcSession = getKcSession();
+  const { domain, realm, admin } = await getClientDomainRealmAdminAndProtocol();
+
+  if (!kcSession || !realm || !admin) {
+    throw new Error(
+      "Error resetting password. Please check if the server is running. and try again."
+    );
+  }
+
+  // check if the user has the "admin" role
+  const decoded = jwtDecode(kcSession) as any;
+  const roles = decoded.realm_access.roles;
+  if (!roles.includes("admin")) {
+    console.log("You need to have the admin role to reset a user's password. Please contact your administrator.", roles)
+    throw new Error(
+      "You need to have the admin role to reset a user's password. Please contact your administrator."
+    );
+  }
+
+  try {
+    const response = await axios.put(`/${admin}/realms/${realm}/users/${userId}/reset-password`, {
+      type: "password",
+      temporary: false,
+      value: password,
+    }, {
+      baseURL: domain,
+      headers: {
+        Authorization: `Bearer ${kcSession}`,
+      },
+    });
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
