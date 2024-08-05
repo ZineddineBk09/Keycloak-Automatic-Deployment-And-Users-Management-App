fd482904eb5b6350107367d7621488383e0b6050
ZineddineBk09 <eino1209@gmail.com>
2024-08-05 09:04:04 +0100

    move users count and title inside data-table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,30 +1,15 @@
-'use client'
-import { useState } from 'react'
-import { columns } from '../../components/users/columns'
-import { DataTable } from '../../components/users/data-table'
-import { UsersContextProvider } from '../../context/users'
-import { getUsersCount } from '../../lib/api/keycloak'
-import { Badge } from "../../components/ui/badge"
+"use client";
+
+import { columns } from "../../components/users/columns";
+import { DataTable } from "../../components/users/data-table";
+import { UsersContextProvider } from "../../context/users";
 
 export default function UsersPage() {
-  const [count, setCount] = useState(0)
-
-  getUsersCount().then((data) => {
-    setCount(data)
-  })
-
   return (
     <>
       <UsersContextProvider>
-        <div className='container mx-auto py-10'>
-          <h1 className='flex items-center gap-x-3 text-3xl font-bold mb-10'>Users
-            <Badge className='font-normal px-3' color='amber'>{
-              new Intl.NumberFormat().format(count)
-            }</Badge>
-          </h1>
-          <DataTable columns={columns} />
-        </div>
+        <DataTable columns={columns} />
       </UsersContextProvider>
     </>
-  )
+  );
 }
