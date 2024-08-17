1bd09ef180a1ff7f44b70f3216dacec896a499e2
ZineddineBk09 <eino1209@gmail.com>
2024-08-17 21:00:28 +0100

    update protected routes

--- routes.ts
+++ routes.ts
@@ -1,3 +1,3 @@
 export const publicRoutes = ["/", "/register"];
 
-export const protectedRoutes = ["/upload", "/users", "/clients", "/settings"];
+export const protectedRoutes = ["/upload", "/users", "/clients", "/settings",'users/[id]'];
